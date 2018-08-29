c6de4a1df361b7f3f0c953fcba286481d921c0e5
lilj <lilj@mchina.cn>
2018-08-29 15:41:15 +0800

    feign add 注解

--- sc-service-feign/src/main/java/com/ant/servicefeign/ServiceFeignApplication.java
+++ sc-service-feign/src/main/java/com/ant/servicefeign/ServiceFeignApplication.java
@@ -7,6 +7,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @EnableFeignClients注解开启Feign的功能
+ *
+ * Feign是一个声明式的伪Http客户端，它使得写Http客户端变得更简单:
+ * 1. Feign 采用的是基于接口的注解
+ * 2. Feign 整合了ribbon，具有负载均衡的能力
+ * 3. 整合了Hystrix，具有熔断的能力
  */
 @EnableEurekaClient
 //@EnableDiscoveryClient
