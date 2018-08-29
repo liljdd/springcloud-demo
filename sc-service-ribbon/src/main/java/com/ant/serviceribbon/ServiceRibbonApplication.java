44ddf51c7d762541fd53d75d65a590a562af8be9
lilj <lilj@mchina.cn>
2018-08-29 15:50:03 +0800

    ribbon使用断路器Hystrix

--- sc-service-ribbon/src/main/java/com/ant/serviceribbon/ServiceRibbonApplication.java
+++ sc-service-ribbon/src/main/java/com/ant/serviceribbon/ServiceRibbonApplication.java
@@ -5,20 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * SpringCLoud中的“Discovery Service”有多种实现，比如：eureka, consul, zookeeper。
- * <p>
  * 1，@EnableDiscoveryClient 注解是基于 spring-cloud-commons依赖，并且在classpath中实现；
  * 2，@EnableEurekaClient 注解是基于 spring-cloud-netflix依赖，只能为eureka作用；
  * 简单的说，就是如果选用的注册中心是eureka，那么就推荐@EnableEurekaClient，
  * 如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient
+ *
+ * @EnableHystrix注解开启Hystrix
  */
 @SpringBootApplication
 @EnableEurekaClient
 //@EnableDiscoveryClient
+@EnableHystrix
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
--- sc-service-ribbon/src/main/java/com/ant/serviceribbon/service/HelloService.java
+++ sc-service-ribbon/src/main/java/com/ant/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.ant.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,23 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * @param name
+     * @return
+     * @HystrixCommand该注解对该方法创建了熔断器的功能，并指定了fallbackMethod熔断方法
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://sc-service-hi/hi?name=" + name, String.class);
+    }
+
+    /**
+     * *熔断方法直接返回了一个字符串，字符串为”hi,”+name+”,sorry,error!”
+     *
+     * @param name
+     * @return
+     */
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 }
