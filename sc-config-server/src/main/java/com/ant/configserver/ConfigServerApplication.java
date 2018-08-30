7a644b96f59774a6ee512d92c8249278fd83de16
lilj <lilj@mchina.cn>
2018-08-30 13:51:22 +0800

    springCloud config

--- sc-config-server/src/main/java/com/ant/configserver/ConfigServerApplication.java
+++ sc-config-server/src/main/java/com/ant/configserver/ConfigServerApplication.java
@@ -3,13 +3,21 @@ package com.ant.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @EnableConfigServer注解开启配置服务器的功能
+ */
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
+    /**
+     * http://localhost:8769/eureka-client/dev
+     */
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
